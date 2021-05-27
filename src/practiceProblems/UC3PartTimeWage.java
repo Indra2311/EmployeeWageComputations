bbeb897359bd78e346f429d9bbc168c0db429a1c
Indra2311 <indrajeet1405@gmail.com>
2021-05-27 18:28:48 +0530

    Modified Part Time Hr in UC3

--- src/practiceProblems/UC3PartTimeWage.java
+++ src/practiceProblems/UC3PartTimeWage.java
@@ -2,39 +2,27 @@ package practiceProblems;
 import java.util.Random;
 
 public class UC3PartTimeWage {
-	private static final int wagePerHour=20;
-	private static int fullDayHour=0;
-	private static double wage;
 	
-	public void empCheck() {
-		Random ran= new Random();
-		
-		final int IS_Present=ran.nextInt(3);
-		
-		if(IS_Present==1) {
-			System.out.println("Employee is Present");
-			fullDayHour=8;
-		} 
-		else if(IS_Present==2) {
-			System.out.println("Employee Worked for Part Time");
-			fullDayHour=4;
+	public static void main(String[] args)
+		{
+		final int IS_FULL_TIME=1;
+		final int IS_PART_TIME=2;
+		final int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+  			double empCheck=Math.floor(Math.random()*10)%3;
+  			if(empCheck == IS_FULL_TIME)
+  			{
+     				empHrs=8;
+  			}
+ 	 		else if(empCheck == IS_PART_TIME)
+  			{
+     				empHrs=4;
+  			}
+  			else
+     				empHrs=0;
+  			empWage=empHrs*EMP_RATE_PER_HOUR;
+  			System.out.println("Employee Wage "+empWage);
 		}
-		else {
-			System.out.println("Employee is absent");
-		}
-		
-		wage= UC3PartTimeWage.dailyWageCalculate(fullDayHour);
-		System.out.println("Wage of the Person is: "+ wage);
-	}
-
-	public static double dailyWageCalculate(int fullDayHour) {
-		return (wagePerHour * fullDayHour);
-	}
-	
-		public static void main(String[] args) {
-			UC3PartTimeWage E1=new UC3PartTimeWage();
-			E1.empCheck();
-		}
-	
 	
 }

a44d2b4ed5382141a3443b88eba70257ec770fbb
Indra2311 <indrajeet1405@gmail.com>
2021-05-27 18:10:43 +0530

    Modified Changes in UC1

--- src/practiceProblems/UC1PresentAbsent.java
+++ src/practiceProblems/UC1PresentAbsent.java
@@ -2,15 +2,16 @@ package practiceProblems;
 
 public class UC1PresentAbsent {
 
-	public static void main(String[] args) {
-	int is_full_time = 1;
-		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == is_full_time)
-		System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
-
-	}
+	public static void main(String[] args)
+		{
+  			int IS_FULL_TIME=1;
+  			double empCheck=Math.floor(Math.random()*10)%2;
+  			if(empCheck == IS_FULL_TIME)
+  			{
+     				System.out.println("Employee is present");
+  			}
+  			else
+     				System.out.println("Employee is absent");
+		}
 
 }

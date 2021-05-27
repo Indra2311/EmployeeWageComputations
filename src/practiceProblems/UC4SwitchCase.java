7c0ecd35734189419290ec86fdc9df282f76c1f8
Indra2311 <indrajeet1405@gmail.com>
2021-05-27 18:44:17 +0530

    Added cases for Employess Hr UC4SwitchCase

--- src/practiceProblems/UC4SwitchCase.java
+++ src/practiceProblems/UC4SwitchCase.java
@@ -1,44 +1,26 @@
 package practiceProblems;
-import java.util.Random;
 
-class UC4SwitchCase {   
-  private static final int wagePerHour=20;
-  private static int fullDayHour=0; 
-  private static double wage;	
-	public void empCheck() {   
-		Random ran=new Random();
-		
-		final int IS_Present=ran.nextInt(3);
-		
-		switch(IS_Present){
-			case 1:
-				System.out.println("Employee is Present");
-						
-				  fullDayHour=8;
-				   break;
-		    case 2:
-				System.out.println("Employee Worked for PartTime");
-					
-				 fullDayHour=4;
-				   break;
-		     case 0:
-				System.out.println("Employee is Absent");
-					break;
-      default:
-					System.out.println("Invalid Value");
-			
+public class UC4SwitchCase {   
+	public static void main(String[] args)
+		{
+		final int IS_FULL_TIME=1;
+		final int IS_PART_TIME=2;
+		final int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+  			double empCheck=Math.floor(Math.random()*10)%3;
+  			switch((int)empCheck)
+  			{
+  			case IS_PART_TIME:
+        			empHrs=4;
+        			break;
+  			case IS_FULL_TIME:
+        			empHrs=8;
+        			break;
+  			default:
+     				empHrs=0;
+  			}
+  			empWage=empHrs*EMP_RATE_PER_HOUR;
+  			System.out.println("Employee Wage "+empWage);
 		}
-		
-		wage=UC4SwitchCase.dailyWageCalculation(fullDayHour);
-			System.out.println("The wage of the Person is: "+wage);
-	}
-	public static double dailyWageCalculation(int fullDayHour){  
-		return (wagePerHour*fullDayHour);
-	}
-
-	public static void main(String[] args) {   
-		UC4SwitchCase E1 =new UC4SwitchCase();
-		E1.empCheck();
-
-	}
 }

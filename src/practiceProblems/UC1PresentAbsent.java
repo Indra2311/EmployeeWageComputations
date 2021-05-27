5eb615407342c1e7e188cec5b688658e7622d1db
Indra2311 <indrajeet1405@gmail.com>
2021-05-27 13:18:18 +0530

    Initial commit for EmployeeWageComputation

---
+++ src/practiceProblems/UC1PresentAbsent.java
@@ -0,0 +1,16 @@
+package practiceProblems;
+
+public class UC1PresentAbsent {
+
+	public static void main(String[] args) {
+	int is_full_time = 1;
+		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == is_full_time)
+		System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+
+	}
+
+}
